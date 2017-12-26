c3b8c372548a6b5991bb7ac06d08ae687d1d9d38
Andrew Stuart <andrew.stuart@supercoders.com.au>
2017-12-27 09:49:15 +1100

    Initial push

---
+++ tinycore_install_oneline_webserver_python/bootrino.sh
@@ -0,0 +1,109 @@
+#!/usr/bin/env sh
+read BOOTRINOJSON <<"BOOTRINOJSONMARKER"
+{
+  "name": "Python 3 one line web server for Tiny Core",
+  "version": "0.0.1",
+  "versionDate": "2017-12-14T09:00:00Z",
+  "description": "Python 3 one line web server for Tiny Core",
+  "options": "",
+  "logoURL": "https://raw.githubusercontent.com/bootrino/bootrinos/master/tinycore_minimal-8.2.1_x86-64/tiny-core-linux-7-logo.png",
+  "readmeURL": "https://raw.githubusercontent.com/bootrino/bootrinos/master/tinycore_wipe_root_disk/README.md",
+  "launchTargetsURL": "https://raw.githubusercontent.com/bootrino/launchtargets/master/defaultLaunchTargetsLatest.json",
+  "websiteURL": "https://github.com/bootrino/",
+  "author": {
+    "url": "https://www.github.com/bootrino",
+    "email": "bootrino@gmail.com"
+  },
+  "tags": [
+    "linux",
+    "runfromram",
+    "tinycore",
+    "python"
+  ]
+}
+BOOTRINOJSONMARKER
+
+setup()
+{
+    export PATH=$PATH:/usr/local/bin:/usr/bin:/usr/local/sbin:/bin
+    OS=tinycore
+    set +xe
+    PACKAGE_NAME="oneline_webserver_python"
+}
+
+download_tinycore_packages()
+{
+    # download the tinycore packages needed
+    URL_BASE=https://raw.githubusercontent.com/bootrino/bootrinos/master/tinycore_install_oneline_webserver_python/
+    mkdir -p /home/tc/${PACKAGE_NAME}_initramfs.src/opt/tce/optional
+    cd /home/tc/${PACKAGE_NAME}_initramfs.src/opt/tce/optional
+    sudo wget -O /home/tc/${PACKAGE_NAME}_initramfs.src/opt/tce/optional/python3.6.tcz ${URL_BASE}python3.6.tcz
+    sudo chmod ug+rx *
+}
+
+make_start_script()
+{
+DIRECTORY=/home/tc/${PACKAGE_NAME}_initramfs.src/opt/bootlocal_enabled/
+mkdir -p ${DIRECTORY}
+cd ${DIRECTORY}
+sudo sh -c 'cat > ${DIRECTORY}60_bootrino_start_oneline_webserver_python' << EOF
+#!/usr/bin/env sh
+# don't crash out if there is an error
+set +xe
+# install the tinycore packages
+# tinycore requires not runnning tce-load as root so we run it as tiny core default user tc
+sudo su - tc -c "tce-load -i /opt/tce/optional/python3.6.tcz"
+
+start_application()
+{
+    echo "Starting oneline_webserver_python...."
+    sudo python3 -m http.server 80 &
+}
+start_application
+EOF
+chmod u=rwx,g=rx,o=rx 60_bootrino_start_nginx
+}
+
+make_index_html()
+{
+DIRECTORY=/home/tc/${PACKAGE_NAME}_initramfs.src/opt/
+mkdir -p ${DIRECTORY}
+cd ${DIRECTORY}
+# make an index.html to serve
+
+sudo sh -c 'cat > ${DIRECTORY}index.html' << EOF
+oneline webserver python says hello world<br/>
+EOF
+chmod u=rwx,g=rx,o=rx index.html
+}
+
+make_initramfs()
+{
+    BOOT_LOCATION=/mnt/boot_partition/
+    cd /home/tc/${PACKAGE_NAME}_initramfs.src
+    find . | cpio -H newc -o | gzip -9 > ${BOOT_LOCATION}${PACKAGE_NAME}_initramfs.gz
+}
+
+append_to_syslinuxcfg()
+{
+sudo sh -c 'cat >> /mnt/boot_partition/syslinux.cfg' << EOF
+    APPEND initrd+=${PACKAGE_NAME}_initramfs.gz
+EOF
+}
+
+setup
+download_tinycore_packages
+make_start_script
+make_index_html
+make_initramfs
+append_to_syslinuxcfg
+
+run_next_bootrino()
+{
+    echo "running next bootrino"
+    cd /bootrino
+    sh /bootrino/runnextbootrino.sh
+}
+run_next_bootrino
+
+
