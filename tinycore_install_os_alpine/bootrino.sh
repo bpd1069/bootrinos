366d4646c2687111b16ecb507b103f14c5d92a49
Andrew Stuart <andrew.stuart@supercoders.com.au>
2018-01-01 17:33:52 +1100

    Initial push

--- tinycore_install_os_alpine/bootrino.sh
+++ tinycore_install_os_alpine/bootrino.sh
@@ -31,15 +31,17 @@ setup()
 
 download_files()
 {
-    ALPINE_ISO_NAME=alpine-virt-3.7.0_rc3-x86_64.iso
+    ROOT_PARTITION=/mnt/root_partition/
+    BOOT_PARTITION=/mnt/boot_partition/
+    ALPINE_ISO_NAME=alpine-virt-3.7.0-x86_64.iso
     ALPINE_ISO_URL=http://dl-cdn.alpinelinux.org/alpine/v3.7/releases/x86_64/
     URL_BASE=https://raw.githubusercontent.com/bootrino/bootrinos/master/tinycore_install_os_alpine/
-    sudo wget -O /mnt/root_partition/${ALPINE_ISO_NAME} ${ALPINE_ISO_URL}${ALPINE_ISO_NAME}
-    sudo wget -O /mnt/boot_partition/rootfs_overlay_initramfs.gz ${URL_BASE}rootfs_overlay_initramfs.gz
+    sudo wget -O ${ROOT_PARTITION}${ALPINE_ISO_NAME} ${ALPINE_ISO_URL}${ALPINE_ISO_NAME}
+    #sudo wget -O ${BOOT_PARTITION}rootfs_overlay_initramfs.gz ${URL_BASE}rootfs_overlay_initramfs.gz
     # COPY OVER THE BOOTRINO DIRECTORY TO THE HARD DISK NEW ROOT PARTITION
-    cd /mnt/root_partition
-    sudo mkdir -p /mnt/root_partition/bootrino/
-    sudo cp -r /bootrino /mnt/root_partition
+    cd ${ROOT_PARTITION}
+    sudo mkdir -p ${ROOT_PARTITION}bootrino/
+    sudo cp -r /bootrino ${ROOT_PARTITION}
     sudo chmod ug+rx *
 }
 
@@ -49,23 +51,25 @@ make_bootrino_initramfsgz()
     HOME_DIR=/home/tc/
     cd ${HOME_DIR}
     sudo find /bootrino | cpio -H newc -o | gzip -9 > ${HOME_DIR}bootrino_initramfs.gz
-    sudo cp ${HOME_DIR}bootrino_initramfs.gz /mnt/boot_partition/bootrino_initramfs.gz
+    sudo cp ${HOME_DIR}bootrino_initramfs.gz ${BOOT_PARTITION}bootrino_initramfs.gz
 }
 
 add_initrd_to_APPEND_in_syslinuxcfg()
 {
-sed -i "/^[[:space:]]*APPEND/ {/ initrd+=${1}/! s/.*/& initrd+=${1}/}" /mnt/boot_partition/boot/syslinux.cfg
+sed -i "/^[[:space:]]*APPEND/ {/ initrd+=${1}/! s/.*/& initrd+=${1}/}" ${BOOT_PARTITION}boot/syslinux.cfg
 }
 
 copy_alpine_from_iso_to_boot()
 {
-    mkdir /mnt/root_partition/alpinefiles
-    mount -o loop alpine-alpine-virt-3.7.0-x86_64.iso /mnt/root_partition/alpinefiles
-    cp -r /mnt/root_partition/source/* /mnt/boot_partition/.
+    sudo mkdir ${ROOT_PARTITION}alpinefiles
+    sudo mount -o loop ${ROOT_PARTITION}alpine-virt-3.7.0-x86_64.iso ${ROOT_PARTITION}alpinefiles
+    sudo mount -o loop ${ROOT_PARTITION}alpine-virt-3.7.0-x86_64.iso ${ROOT_PARTITION}alpinefiles/
+    sudo cp -r ${ROOT_PARTITION}source/* ${BOOT_PARTITION}.
 }
 
 setup
 download_files
+copy_alpine_from_iso_to_boot
 #add_initrd_to_APPEND_in_syslinuxcfg "rootfs_overlay_initramfs.gz"
 #make_bootrino_initramfsgz
 #add_initrd_to_APPEND_in_syslinuxcfg "bootrino_initramfs.gz"
