da292c4fd43c698e45b9689fc408913571efe5d8
Andrew Stuart <andrew.stuart@supercoders.com.au>
2018-01-07 14:46:31 +1100

    Initial push

--- tinycore_load_over_ubuntu/bootrino.sh
+++ tinycore_load_over_ubuntu/bootrino.sh
@@ -22,15 +22,20 @@ read BOOTRINOJSON <<"BOOTRINOJSONMARKER"
   ]
 }
 BOOTRINOJSONMARKER
-URL_BASE=https://raw.githubusercontent.com/bootrino/bootrinos/master/tinycore_minimal-8.2.1_x86-64/
 # THIS SCRIPT RUNS ON UBUNTU AND REPLACES THE OPERATING SYSTEM WITH TINYCORE!
 OS=ubuntu
 
 # download the operating system files for tinycore
 cd /boot
+
+# get the Tiny Core OS files
+URL_BASE=https://raw.githubusercontent.com/bootrino/bootrinos/master/tinycore_minimal-8.2.1_x86-64/
 /usr/bin/wget -O /boot/vmlinuz64 ${URL_BASE}vmlinuz64
 /usr/bin/wget -O /boot/corepure64.gz ${URL_BASE}corepure64.gz
+
+# get the initramfs overlay files
 /usr/bin/wget -O /boot/rootfs_overlay_initramfs.gz ${URL_BASE}rootfs_overlay_initramfs.gz
+/usr/bin/wget -O /boot/wipe_root_disk_initramfs.gz ${URL_BASE}wipe_root_disk_initramfs.gz
 
 # copy the Ubuntu network configuration into a ramfs which will then be available in tinycore when it boots up
 mkdir -p /bootrino
@@ -53,7 +58,7 @@ set timeout=1
 GRUB_TIMEOUT=1
 menuentry 'tinycore 64' {
 linux /boot/vmlinuz64 root=LABEL=cloudimg-rootfs tce=/opt/tce noswap modules=ext4  console=tty1 console=ttyS0
-initrd /boot/corepure64.gz /boot/rootfs_overlay_initramfs.gz /boot/bootrino_initramfs.gz
+initrd /boot/corepure64.gz /boot/rootfs_overlay_initramfs.gz /boot/bootrino_initramfs.gz /boot/wipe_root_disk_initramfs.gz
 }
 EOFMARKER
 
